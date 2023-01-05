b858cdfa725e0e68d9498a6f2b302aaf5b1635a2
Mathias Buus <mathiasbuus@gmail.com>
2023-01-05 23:45:01 +0100

    use path

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,5 @@
+const path = require('tiny-paths')
+
 {
   // TODO: can we easily extend Uint8Arrays like in node?
 
@@ -94,6 +96,7 @@ class Module {
 
     function require (req) {
       if (req === 'module') return Module
+      if (req === 'path') return path
       if (req.endsWith('.node')) return process.addon(req, { resolve: false }) // node compat
       const filename = resolve(req)
       return Module.load(filename)
@@ -105,83 +108,12 @@ class Module {
   static load (filename) {
     if (Module.cache[filename]) return Module.cache[filename].exports
 
-    const mod = new Module(filename, Module.resolvePath(filename, '..'))
+    const mod = new Module(filename, path.dirname(filename))
 
     Module.cache[filename] = mod
 
     return filename.endsWith('.json') ? mod._loadJSON() : mod._loadJS()
   }
-
-  static resolvePath = (function () { // inlined unix-path-resolve
-    return resolve
-
-    function parse (addr) {
-      const names = addr.split(/[/\\]/)
-
-      const r = {
-        isAbsolute: false,
-        names
-      }
-
-      // don't think this ever happens, but whatevs
-      if (names.length === 0) return r
-
-      if (names.length > 1 && names[0].endsWith(':')) {
-        r.isAbsolute = true
-
-        if (names[0].length === 2) { // windows
-          r.names = names.slice(1)
-          return r
-        }
-
-        if (names[0] === 'file:') {
-          r.names = names.slice(1)
-          return r
-        }
-
-        r.names = names.slice(3)
-        return r
-      }
-
-      r.isAbsolute = addr.startsWith('/') || addr.startsWith('\\')
-
-      return r
-    }
-
-    function resolve (a, b = '') {
-      const ap = parse(a)
-      const bp = parse(b)
-
-      if (bp.isAbsolute) {
-        return resolveNames([], bp.names)
-      }
-
-      if (!ap.isAbsolute) {
-        throw new Error('One of the two paths must be absolute')
-      }
-
-      return resolveNames(ap.names, bp.names)
-    }
-
-    function toString (p, names) {
-      for (let i = 0; i < names.length; i++) {
-        if (names[i] === '') continue
-        if (names[i] === '.') continue
-        if (names[i] === '..') {
-          if (p.length === 1) throw new Error('Path cannot be resolved, too many \'..\'')
-          p = p.slice(0, p.lastIndexOf('/')) || '/'
-          continue
-        }
-        p += (p.length === 1 ? names[i] : '/' + names[i])
-      }
-
-      return p
-    }
-
-    function resolveNames (a, b) {
-      return toString(toString('/', a), b)
-    }
-  })()
 
   static runScript (module, source, require) {
     new Function('__dirname', '__filename', 'module', 'exports', 'require', source + '\n//# sourceURL=' + module.filename)( // eslint-disable-line
@@ -196,17 +128,17 @@ class Module {
   static resolve (req, dirname) {
     if (req.length === 0) throw new Error('Could not resolve ' + req + ' from ' + dirname)
 
-    let path = null
+    let p = null
 
     if (req[0] !== '.' && req[0] !== '/') {
       const target = 'node_modules/' + req
 
       while (true) {
-        const nm = Module.resolvePath(dirname, target)
+        const nm = path.resolve(dirname, target)
         const type = process._existsSync(nm)
 
         if (type === 0) {
-          const parent = Module.resolvePath(dirname, '..')
+          const parent = path.dirname(dirname)
           if (parent === nm) throw new Error('Could not resolve ' + req + ' from ' + dirname)
           dirname = parent
           continue
@@ -218,44 +150,44 @@ class Module {
       }
     }
 
-    path = Module.resolvePath(dirname, req)
+    p = path.resolve(dirname, req)
 
-    if (/\.(js|mjs|cjs|json)$/i.test(req) && process._existsSync(path) === 1) {
-      return path
+    if (/\.(js|mjs|cjs|json)$/i.test(req) && process._existsSync(p) === 1) {
+      return p
     }
 
-    if (process._existsSync(path + '.js') === 1) {
-      return path + '.js'
+    if (process._existsSync(p + '.js') === 1) {
+      return p + '.js'
     }
 
-    if (process._existsSync(path + '.cjs') === 1) {
-      return path + '.cjs'
+    if (process._existsSync(p + '.cjs') === 1) {
+      return p + '.cjs'
     }
 
-    if (process._existsSync(path + '.mjs') === 1) {
-      return path + '.mjs'
+    if (process._existsSync(p + '.mjs') === 1) {
+      return p + '.mjs'
     }
 
-    if (process._existsSync(path + '.json') === 1) {
-      return path + '.json'
+    if (process._existsSync(p + '.json') === 1) {
+      return p + '.json'
     }
 
-    const pkg = Module.resolvePath(path, 'package.json')
+    const pkg = path.resolve(p, 'package.json')
 
     if (process._existsSync(pkg) === 1) {
       const json = Module.load(pkg)
-      path = Module.resolvePath(path, json.main || 'index.js')
+      p = path.resolve(p, json.main || 'index.js')
 
-      if (process._existsSync(path) !== 1) {
+      if (process._existsSync(p) !== 1) {
         throw new Error('Could not resolve ' + req + ' from ' + dirname)
       }
 
-      return path
+      return p
     }
 
-    path = Module.resolvePath(path, 'index.js')
-    if (process._existsSync(path) === 1) {
-      return path
+    p = path.resolve(p, 'index.js')
+    if (process._existsSync(p) === 1) {
+      return p
     }
 
     throw new Error('Could not resolve ' + req + ' from ' + dirname)
@@ -369,4 +301,4 @@ class Module {
   }
 }
 
-process.main = Module.load(process._entryPoint, Module.resolvePath(process._entryPoint, '..'))
+process.main = Module.load(process._entryPoint, path.dirname(process._entryPoint))
