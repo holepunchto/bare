d9ecb1bd74b554cf334adc3db95fb7dc34083512
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-01-05 10:21:01 +0100

    Build `bootstrap.h` using esbuild (#2)

---
+++ scripts/build.mjs
@@ -0,0 +1,17 @@
+import fs from 'fs/promises'
+import esbuild from 'esbuild'
+import includeStatic from 'include-static'
+
+const result = await esbuild.build({
+  entryPoints: [
+    'src/bootstrap.js'
+  ],
+  bundle: true,
+  minify: true,
+  write: false,
+  outfile: 'src/bootstrap.h'
+})
+
+for (const file of result.outputFiles) {
+  await fs.writeFile(file.path, includeStatic('pearjs_bootstrap', file.contents));
+}
