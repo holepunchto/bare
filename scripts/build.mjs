873fa84266094bc6d7df0a6d35b3499c8dc055eb
Mathias Buus <mathiasbuus@gmail.com>
2023-01-17 13:58:15 +0100

    move to script-linker

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,3 +1,4 @@
+import ScriptLinker from 'script-linker'
 import childProcess from 'child_process'
 import fs from 'fs/promises'
 import path from 'path'
@@ -7,55 +8,32 @@ import includeStatic from 'include-static'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const root = path.dirname(__dirname)
 
-const root = path.join(__dirname, '..')
-
-const dirnamePlugin = {
-  name: 'dirname-plugin',
-  setup (build) {
-    build.onLoad({ filter: /\.js$/, namespace: '' }, async (args) => {
-      const src = await fs.readFile(args.path, 'utf-8')
-
-      // const filename = path.resolve('/', path.relative(root, args.path))
-      // const dirname = path.resolve('/', path.relative(root, path.dirname(args.path)))
-
-      // TODO: name the anon paths when that works with libnapi etc
-      const filename = args.path
-      const dirname = path.dirname(args.path)
-
-      return {
-        contents: 'const __dirname = ' + JSON.stringify(dirname) + ', __filename = ' + JSON.stringify(filename) + ';' + src,
-        loader: 'js'
-      }
-    })
-  }
-}
-
-const result = await esbuild.build({
-  plugins: [dirnamePlugin],
-  entryPoints: [
-    path.join(root, 'lib/bootstrap.js')
-  ],
-  alias: {
-    'node-gyp-build': path.join(root, 'lib/load-addon.js')
+const s = new ScriptLinker({
+  bare: true,
+  map (path) {
+    return '<pearjs>' + path
   },
-  bundle: true,
-  write: false,
-  outfile: path.join(root, 'build/bootstrap.js'),
-  banner: {
-    js: '(function (pear) {'
+  mapResolve (req, basedir) {
+    if (req === 'node-gyp-build') return '/lib/load-addon.js'
+    return req
   },
-  footer: {
-    js: '})\n//# sourceURL=<pearjs>/bootstrap.js'
+  readFile (filename) {
+    return fs.readFile(path.join(root, filename))
   }
 })
 
-for (const file of result.outputFiles) {
-  await fs.mkdir(path.dirname(file.path), { recursive: true })
+const bundle = await s.bundle('/lib/bootstrap.js', {
+  header: '(function (pear) {',
+  footer: '})',
+})
 
-  await fs.writeFile(file.path, file.contents)
-  await fs.writeFile(file.path.replace(/\.js$/, '.h'), includeStatic('pear_bootstrap', file.contents))
-}
+await fs.mkdir(path.dirname(path.join(root, 'build')), { recursive: true })
+
+// just for debugging write the js file
+await fs.writeFile(path.join(root, 'build/bootstrap.js'), bundle)
+await fs.writeFile(path.join(root, 'build/bootstrap.h'), includeStatic('pear_bootstrap', Buffer.from(bundle)))
 
 let proc = null
 
