d90614bcc9425543330980a658b8ddd12c26cbaa
Mathias Buus <mathiasbuus@gmail.com>
2023-01-26 12:33:44 +0100

    update bench

--- bench/buffer.js
+++ bench/buffer.js
@@ -5,24 +5,41 @@ for (let n = 0; n < 3; n++) {
   console.log()
   console.log('Round ' + n)
 
-  console.time('Buffer.allocUnsafe')
-  for (let i = 0; i < 1e6; i++) {
-    Buffer.allocUnsafe(65536)
+  {
+    console.time('Buffer.allocUnsafe')
+    for (let i = 0; i < 1e6; i++) {
+      Buffer.allocUnsafe(65536)
+    }
+    console.timeEnd('Buffer.allocUnsafe')
   }
-  console.timeEnd('Buffer.allocUnsafe')
 
-  console.time('Buffer.alloc')
-  for (let i = 0; i < 1e6; i++) {
-    Buffer.alloc(65536)
+  {
+    console.time('Buffer.alloc')
+    for (let i = 0; i < 1e6; i++) {
+      Buffer.alloc(65536)
+    }
+    console.timeEnd('Buffer.alloc')
   }
-  console.timeEnd('Buffer.alloc')
 
-  const a = Buffer.from('foobarz')
-  const b = Buffer.from('foobary')
+  {
+    const a = Buffer.from('foobarz')
+    const b = Buffer.from('foobary')
 
-  console.time('buf.compare(otherBuf)')
-  for (let i = 0; i < 1e6; i++) {
-    a.compare(b)
+    console.time('smallBuf.compare(otherSmallBuf)')
+    for (let i = 0; i < 1e6; i++) {
+      a.compare(b)
+    }
+    console.timeEnd('smallBuf.compare(otherSmallBuf)')
   }
-  console.timeEnd('buf.compare(otherBuf)')
+
+  {
+    const a = Buffer.alloc(42424, 'foobarz')
+    const b = Buffer.alloc(42424, 'foobary')
+
+    console.time('bigBuf.compare(otherBigBuf)')
+    for (let i = 0; i < 1e6; i++) {
+      a.compare(b)
+    }
+    console.timeEnd('bigBuf.compare(otherBigBuf)')
+  }
 }
