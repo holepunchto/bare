35f2ca6164076daac7b90d1b3f35b58def6d276f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-01-31 11:41:34 +0100

    Add `--debug` flag to build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,15 +1,30 @@
 import path from 'path'
 import url from 'url'
 import childProcess from 'child_process'
+import minimist from 'minimist'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const root = path.join(__dirname, '..')
 
+const argv = minimist(process.argv.slice(2), {
+  boolean: [
+    'debug'
+  ]
+})
+
+const buildType = argv.debug ? 'Debug' : 'Release'
+
 let proc = null
 
-proc = childProcess.spawnSync('cmake', ['-S', '.', '-B', 'build', '-DCMAKE_BUILD_TYPE=Release'], {
+const args = ['-S', '.', '-B', 'build', `-DCMAKE_BUILD_TYPE=${buildType}`]
+
+if (argv.debug) {
+  args.push('-DPEAR_ENABLE_ASAN=ON')
+}
+
+proc = childProcess.spawnSync('cmake', args, {
   cwd: root,
   stdio: 'inherit'
 })
