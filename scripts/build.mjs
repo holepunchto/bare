abcc52b51e923e5f16103fc7abf67072be87919a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-01 14:39:54 +0100

    Use `@pearjs/dev` for builds

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,12 +1,12 @@
 import path from 'path'
 import url from 'url'
-import childProcess from 'child_process'
 import minimist from 'minimist'
+import dev from '@pearjs/dev'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const root = path.join(__dirname, '..')
+const cwd = path.join(__dirname, '..')
 
 const argv = minimist(process.argv.slice(2), {
   boolean: [
@@ -14,26 +14,6 @@ const argv = minimist(process.argv.slice(2), {
   ]
 })
 
-const buildType = argv.debug ? 'Debug' : 'Release'
+dev.configure({ cwd, debug: argv.debug })
 
-let proc = null
-
-const args = ['-S', '.', '-B', 'build', `-DCMAKE_BUILD_TYPE=${buildType}`]
-
-if (argv.debug) {
-  args.push('-DPEAR_ENABLE_ASAN=ON')
-}
-
-proc = childProcess.spawnSync('cmake', args, {
-  cwd: root,
-  stdio: 'inherit'
-})
-
-if (proc.status) process.exit(proc.status)
-
-proc = childProcess.spawnSync('cmake', ['--build', 'build'], {
-  cwd: root,
-  stdio: 'inherit'
-})
-
-if (proc.status) process.exit(proc.status)
+dev.build({ cwd })
