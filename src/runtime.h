d85408f324c1fb532603dae88ed5892253523291
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-08 10:25:47 +0100

    Don't offset bootstrap

--- src/runtime.h
+++ src/runtime.h
@@ -564,7 +564,7 @@ pear_runtime_setup (pear_t *pear) {
   js_create_string_utf8(env, (const char *) pear_bootstrap_js, pear_bootstrap_js_len, &script);
 
   js_value_t *bootstrap;
-  err = js_run_script(env, "<pear>/bootstrap.js", -1, 1, script, &bootstrap);
+  err = js_run_script(env, "<pear>/bootstrap.js", -1, 0, script, &bootstrap);
   if (err < 0) return trigger_fatal_exception(env);
 
   err = js_call_function(env, global, bootstrap, 1, &exports, NULL);
