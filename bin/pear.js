cb143f42f6ec5cacc8b2c4bd33de9d4c7d755105
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-23 13:49:02 +0100

    Initial support for CLI flags

--- bin/pear.js
+++ bin/pear.js
@@ -1,6 +1,25 @@
 const Module = require('module')
 const path = require('path')
 
+const argv = require('minimist')(process.argv, {
+  boolean: [
+    'version'
+  ],
+  alias: {
+    version: 'v'
+  }
+})
+
+process.argv = argv._
+
+if (argv.version) {
+  const pkg = require('../package.json')
+
+  console.log(`v${pkg.version}`)
+
+  process.exit()
+}
+
 Module.load(
   process.argv[1] = Module.resolve(
     path.resolve(process.cwd(), process.argv[1])
