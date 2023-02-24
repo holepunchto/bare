f5a8321dd423d11c5452349d257401f1aae76e16
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-24 10:04:05 +0100

    Sort builtins

--- src/builtins/module.js
+++ src/builtins/module.js
@@ -1,10 +1,10 @@
 const Module = module.exports = require('@pearjs/module')
 
-Module._builtins.module = require('./module')
 Module._builtins.assert = require('./assert')
 Module._builtins.buffer = require('./buffer')
 Module._builtins.console = require('./console')
 Module._builtins.events = require('./events')
+Module._builtins.module = require('./module')
 Module._builtins.path = require('./path')
 Module._builtins.process = require('./process')
 Module._builtins.timers = require('./timers')
