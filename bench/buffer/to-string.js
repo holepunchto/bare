949e50ce7568b236fdf2ce44235bec86cf0ef73a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-27 19:24:35 +0100

    Add `buffer.toString()` bench

---
+++ bench/buffer/to-string.js
@@ -0,0 +1,11 @@
+console.log(process.execPath)
+
+const buffer = Buffer.alloc(65536, 'a')
+
+console.time('buffer.toString()')
+
+for (let i = 0; i < 1e6; i++) {
+  buffer.toString()
+}
+
+console.timeEnd('buffer.toString()')
