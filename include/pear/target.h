4db1cdfe8745d9d0ee5306b4477caf770a357d05
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-07 12:43:16 +0100

    Fix platform names and add additional defines

--- include/pear/target.h
+++ include/pear/target.h
@@ -5,27 +5,36 @@
 #include <TargetConditionals.h>
 #if TARGET_OS_IOS
 #define PEAR_PLATFORM "ios"
+#define PEAR_PLATFORM_IOS
 #else
 #define PEAR_PLATFORM "darwin"
+#define PEAR_PLATFORM_DARWIN
 #endif
 #elif defined(__linux__)
 #define PEAR_PLATFORM "linux"
+#define PEAR_PLATFORM_LINUX
 #elif defined(__ANDROID__)
-#define PLATFORM_NAME "android"
+#define PEAR_PLATFORM "android"
+#define PEAR_PLATFORM_ANDROID
 #elif defined(_WIN32)
-#define PLATFORM_NAME "windows"
+#define PEAR_PLATFORM "win32"
+#define PEAR_PLATFORM_WINDOWS
 #else
 #error Unsupported platform
 #endif
 
 #if defined(__aarch64__)
 #define PEAR_ARCH "arm64"
+#define PEAR_ARCH_ARM64
 #elif defined(__arm__)
 #define PEAR_ARCH "arm"
+#define PEAR_ARCH_ARM
 #elif defined(__x86_64)
 #define PEAR_ARCH "x64"
+#define PEAR_ARCH_X64
 #elif defined(__i386__)
 #define PEAR_ARCH "ia32"
+#define PEAR_ARCH_IA32
 #else
 #error Unsupported architecture
 #endif
