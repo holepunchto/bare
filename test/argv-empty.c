dc9eee8807306ad17ae4ef69836116d520f246f8
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-07 12:07:16 +0100

    Test setup with empty `argv`

---
+++ test/argv-empty.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <pear.h>
+#include <uv.h>
+
+int
+main (int argc, char *argv[]) {
+  argc = 0;
+  argv = NULL;
+
+  argv = uv_setup_args(argc, argv);
+
+  int e;
+
+  pear_t pear;
+  e = pear_setup(uv_default_loop(), &pear, argc, argv);
+  assert(e == 0);
+
+  int exit_code;
+  e = pear_teardown(&pear, &exit_code);
+  assert(e == 0);
+
+  return exit_code;
+}
