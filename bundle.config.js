2f082e75659fb2e8ddc52324dc80be1deb82269d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-10 12:45:16 +0100

    Update `@pearjs/module` and add core import map

--- bundle.config.js
+++ bundle.config.js
@@ -3,6 +3,7 @@ module.exports = {
   format: 'js',
   target: 'c',
   name: 'pear_bundle',
+  importMap: require.resolve('./src/import-map.json'),
   header: '(function (pear) {',
   footer: '})'
 }
