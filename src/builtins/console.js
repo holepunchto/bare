adf08592559525098dbc575adc03aa1734862257
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-05-17 10:41:11 +0200

    Replace NULL byte before printing

--- src/builtins/console.js
+++ src/builtins/console.js
@@ -4,10 +4,10 @@ const Console = require('bare-console')
 
 global.console = module.exports = exports = new Console({
   stdout (data) {
-    bare.printInfo(data)
+    bare.printInfo(data.replace(/\u0000/g, '\\x00'))
   },
   stderr (data) {
-    bare.printError(data)
+    bare.printError(data.replace(/\u0000/g, '\\x00'))
   }
 })
 
