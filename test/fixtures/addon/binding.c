68076f9712fc6a6d7223183650476733eee50876
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-05-22 14:01:17 +0200

    Use `libutf` types

--- test/fixtures/addon/binding.c
+++ test/fixtures/addon/binding.c
@@ -1,10 +1,11 @@
 #include <assert.h>
 #include <bare.h>
 #include <js.h>
+#include <utf.h>
 
 static js_value_t *
 init (js_env_t *env, js_value_t *exports) {
-  int err = js_create_string_utf8(env, "Hello from addon", -1, &exports);
+  int err = js_create_string_utf8(env, (utf8_t *) "Hello from addon", -1, &exports);
   assert(err == 0);
 
   return exports;
