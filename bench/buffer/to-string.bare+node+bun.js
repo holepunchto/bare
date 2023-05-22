7043a8e785f552c8531a2fe261a6025eccc33690
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-05-22 15:44:18 +0200

    Expand buffer encoding benchmark

--- bench/buffer/to-string.bare+node+bun.js
+++ bench/buffer/to-string.bare+node+bun.js
@@ -4,6 +4,18 @@ require('../harness')
 
 const buffer = Buffer.alloc(65536, 'a')
 
-bench('buffer.toString()', () => {
+bench('buffer.toString(\'utf8\')', () => {
   buffer.toString()
+})
+
+bench('buffer.toString(\'utf16le\')', () => {
+  buffer.toString('utf16le')
+})
+
+bench('buffer.toString(\'hex\')', () => {
+  buffer.toString('hex')
+})
+
+bench('buffer.toString(\'base64\')', () => {
+  buffer.toString('base64')
 })
--- bench/buffer/to-string.deno.js
+++ bench/buffer/to-string.deno.js
@@ -6,6 +6,18 @@ import '../harness.js'
 
 const buffer = Buffer.alloc(65536, 'a')
 
-bench('buffer.toString()', () => {
+bench('buffer.toString(\'utf8\')', () => {
   buffer.toString()
+})
+
+bench('buffer.toString(\'utf16le\')', () => {
+  buffer.toString('utf16le')
+})
+
+bench('buffer.toString(\'hex\')', () => {
+  buffer.toString('hex')
+})
+
+bench('buffer.toString(\'base64\')', () => {
+  buffer.toString('base64')
 })
