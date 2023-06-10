055da4121beaac7bdaedc84c6a3749d694bc648b
Mathias Buus <mathiasbuus@gmail.com>
2023-06-10 16:33:16 +0200

    disable addon normalisation as it breaks loading multiple majors

--- src/builtins/process/addon.js
+++ src/builtins/process/addon.js
@@ -1,11 +1,7 @@
 /* global bare */
 
-const path = require('../path')
-
 module.exports = exports = function addon (specifier) {
   if (typeof specifier !== 'string') throw new TypeError('dirname must be a string')
-
-  specifier = normalize(specifier)
 
   if (exports.cache[specifier]) return exports.cache[specifier]
 
@@ -19,15 +15,5 @@ exports.cache = Object.create(null)
 exports.resolve = function resolve (specifier) {
   if (typeof specifier !== 'string') throw new TypeError('dirname must be a string')
 
-  specifier = normalize(specifier)
-
   return bare.resolveAddon(specifier)
-}
-
-function normalize (specifier) {
-  const i = specifier.indexOf(path.join('/', 'node_modules'))
-
-  if (i >= 0) specifier = path.join(process.cwd(), specifier.slice(i))
-
-  return specifier
 }
