2e3a1bcb0eafbda12e8da353fda468b433a3ac9a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-06-22 12:14:41 +0200

    Use file descriptors for logging on POSIX

--- src/runtime/posix.h
+++ src/runtime/posix.h
@@ -9,7 +9,7 @@ bare_runtime__print_info (const char *format, ...) {
   va_list args;
   va_start(args, format);
 
-  int err = vfprintf(stdout, format, args);
+  int err = vdprintf(1, format, args);
 
   va_end(args);
 
@@ -21,7 +21,7 @@ bare_runtime__print_error (const char *format, ...) {
   va_list args;
   va_start(args, format);
 
-  int err = vfprintf(stderr, format, args);
+  int err = vdprintf(2, format, args);
 
   va_end(args);
 
