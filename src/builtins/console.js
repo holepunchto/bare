2b219ac9f924bfbc5577758a46c390bc363901de
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-08-10 13:13:34 +0200

    Make sure `console` methods are bound

--- src/builtins/console.js
+++ src/builtins/console.js
@@ -3,9 +3,12 @@
 const Console = require('bare-console')
 
 global.console = module.exports = exports = new Console({
+  bind: true,
+
   stdout (data) {
     bare.printInfo(data.replace(/\u0000/g, '\\x00')) // eslint-disable-line no-control-regex
   },
+
   stderr (data) {
     bare.printError(data.replace(/\u0000/g, '\\x00')) // eslint-disable-line no-control-regex
   }
