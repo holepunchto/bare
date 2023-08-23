2fb95378719f55aa5c3bbde9584237f7f23875c3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-08-23 09:50:19 +0200

    Move imports to bundle config

--- src/bundle.config.js
+++ src/bundle.config.js
@@ -4,7 +4,18 @@ module.exports = {
   target: 'c',
   name: 'bare_bundle',
   builtins: [],
-  importMap: require.resolve('./import-map.json'),
+  imports: {
+    assert: '/src/builtins/assert.js',
+    buffer: '/src/builtins/buffer.js',
+    console: '/src/builtins/console.js',
+    events: '/src/builtins/events.js',
+    module: '/src/builtins/module.js',
+    os: '/src/builtins/os.js',
+    path: '/src/builtins/path.js',
+    process: '/src/builtins/process.js',
+    timers: '/src/builtins/timers.js',
+    url: '/src/builtins/url.js'
+  },
   header: '(function (bare) {',
   footer: '})',
   compress: ['*']
