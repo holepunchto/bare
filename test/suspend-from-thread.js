a933ec4bd68c00e41aa8867f757e23a24138866e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-08-23 21:20:35 +0200

    Make suspend tests more noisy

--- test/suspend-from-thread.js
+++ test/suspend-from-thread.js
@@ -1,6 +1,17 @@
+const assert = require('assert')
 const Thread = require('thread')
 
-process.on('suspend', () => process.resume())
+process
+  .on('suspend', () => {
+    console.log('emit suspend')
+    process.resume()
+  })
+  .on('idle', () => {
+    assert(false, 'Should not idle')
+  })
+  .on('resume', () => {
+    console.log('emit resume')
+  })
 
 const thread = new Thread(() => process.suspend())
 
--- test/suspend-resume-from-thread.js
+++ test/suspend-resume-from-thread.js
@@ -1,7 +1,14 @@
 const Thread = require('thread')
 
 process
-  .on('idle', () =>
-    new Thread(() => process.resume())
-  )
+  .on('suspend', () => {
+    console.log('emit suspend')
+  })
+  .on('idle', () => {
+    console.log('emit idle')
+    new Thread(() => process.resume()) // eslint-disable-line no-new
+  })
+  .on('resume', () => {
+    console.log('emit resume')
+  })
   .suspend()
