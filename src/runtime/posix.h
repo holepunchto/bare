6bf2e9df53dd3af4f4369c2c6fbaf4b6cdee5ed0
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-10-16 16:00:44 +0200

    Fix printing on Windows

--- src/runtime/posix.h
+++ src/runtime/posix.h
@@ -9,7 +9,7 @@ bare_runtime__print_info (const char *format, ...) {
   va_list args;
   va_start(args, format);
 
-  int err = vdprintf(1, format, args);
+  int err = vfprintf(stdout, format, args);
 
   va_end(args);
 
@@ -21,7 +21,7 @@ bare_runtime__print_error (const char *format, ...) {
   va_list args;
   va_start(args, format);
 
-  int err = vdprintf(2, format, args);
+  int err = vfprintf(stderr, format, args);
 
   va_end(args);
 
