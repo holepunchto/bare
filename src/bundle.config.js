f33f3bceac548390098ee081bf7e0f34854d6c8e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-11-15 13:25:46 +0100

    Update `bare-dev`

--- src/bundle.config.js
+++ src/bundle.config.js
@@ -3,7 +3,16 @@ module.exports = {
   format: 'js',
   target: 'c',
   name: 'bare_js',
-  header: '(function (bare) {',
+  header: `
+    (function (bare) {
+      const require = () => {
+        throw new Error('Cannot require modules from bootstrap script')
+      }
+
+      require.addon = (specifier) => {
+        return bare.addon(specifier)
+      }
+  `,
   footer: '})',
   compress: ['*']
 }
