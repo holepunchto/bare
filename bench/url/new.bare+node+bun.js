6df3fa2b8bf96a1da0d5cb12bc8f2603c67d988e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-02-13 14:15:27 +0100

    Make Standard happy

--- bench/url/new.bare+node+bun.js
+++ bench/url/new.bare+node+bun.js
@@ -3,5 +3,5 @@
 require('../harness')
 
 bench('new URL()', () => {
-  new URL('https://example.com/hello/world?query=string#fragment')
+  new URL('https://example.com/hello/world?query=string#fragment') // eslint-disable-line no-new
 })
