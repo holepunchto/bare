c5db747ceac0a50921759b22b98beeb4c22b4f5a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-03-07 20:20:56 +0100

    Make sure addon registration symbol has external linkage

--- include/bare/helper.h
+++ include/bare/helper.h
@@ -6,4 +6,12 @@
 #define BARE_STRING_LITERAL(x) #x
 #define BARE_STRING(x)         BARE_STRING_LITERAL(x)
 
+#ifdef __cplusplus
+#define BARE_EXTERN_C_START extern "C" {
+#define BARE_EXTERN_C_END }
+#else
+#define BARE_EXTERN_C_START
+#define BARE_EXTERN_C_END
+#endif
+
 #endif // BARE_HELPER_H
