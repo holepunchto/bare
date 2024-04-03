23f268c55a02bbd559d9f841b433f7cdf50f0566
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-04-03 11:57:48 +0200

    Add `structuredClone()` benchmark

---
+++ bench/platform/structured-clone.bare+node+bun.js
@@ -0,0 +1,17 @@
+/* global bench */
+
+require('../harness')
+
+const object = {
+  string: 'hello world',
+  number: 42,
+  boolean: true,
+  array: ['element'],
+  object: {
+    key: 'value'
+  }
+}
+
+bench('structuredClone()', () => {
+  structuredClone(object)
+})
