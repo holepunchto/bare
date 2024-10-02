4169a9ba4f8589dfd45176f704ac940225549eb0
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-02 11:08:30 +0200

    Formatting

--- src/globals.js
+++ src/globals.js
@@ -12,14 +12,15 @@
  */
 
 const resolved = Promise.resolve()
-const crashError = err => setImmediate(() => { throw err })
+
+const crash = err => setImmediate(() => { throw err })
 
 global.queueMicrotask = function queueMicrotask (fn) {
   resolved
     .then(fn)
     // Make sure that exceptions are reported as normal uncaughts, not promise
     // rejections.
-    .catch(crashError)
+    .catch(crash)
 }
 
 /**
