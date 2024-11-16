d6dfba67740f147e76e2adcfe38591623b5259cf
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-16 20:31:05 +0100

    Update test addon

--- test/fixtures/addon/binding.c
+++ test/fixtures/addon/binding.c
@@ -4,11 +4,11 @@
 #include <utf.h>
 
 static js_value_t *
-init (js_env_t *env, js_value_t *exports) {
+addon_exports (js_env_t *env, js_value_t *exports) {
   int err = js_create_string_utf8(env, (utf8_t *) "Hello from addon", -1, &exports);
   assert(err == 0);
 
   return exports;
 }
 
-BARE_MODULE(test_binding, init)
+BARE_MODULE(addon, addon_exports)
