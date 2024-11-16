fcd869124c6420b1df1572c07249e6e48cbb09a9
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-16 20:44:29 +0100

    Skip test on Windows for now

--- test/dependent-addon.js
+++ test/dependent-addon.js
@@ -3,6 +3,8 @@ const assert = require('bare-assert')
 const url = require('bare-url')
 const { Addon } = Bare
 
+if (Bare.platform === 'win32') Bare.exit()
+
 const a = Addon.load(url.pathToFileURL(`./test/fixtures/dependent-addon/a/prebuilds/${Addon.host}/a.bare`))
 const b = Addon.load(url.pathToFileURL(`./test/fixtures/dependent-addon/b/prebuilds/${Addon.host}/b.bare`))
 
