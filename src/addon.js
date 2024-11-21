f02719cfffe8d2d1cd62aa82ea9fe2945e6cad37
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-21 09:49:27 +0100

    Fix formatting

--- src/addon.js
+++ src/addon.js
@@ -5,209 +5,206 @@ const resolve = require('bare-addon-resolve')
 const { fileURLToPath } = require('bare-url')
 const { AddonError } = require('./errors')
 
-const Addon =
-  (module.exports =
-  exports =
-    class Addon {
-      constructor(url) {
-        this._url = url
-        this._exports = {}
-        this._name = null
-        this._handle = null
+module.exports = exports = class Addon {
+  constructor(url) {
+    this._url = url
+    this._exports = {}
+    this._name = null
+    this._handle = null
 
-        Object.preventExtensions(this)
+    Object.preventExtensions(this)
 
-        Addon._addons.add(this)
-      }
+    Addon._addons.add(this)
+  }
 
-      get url() {
-        return this._url
-      }
+  get url() {
+    return this._url
+  }
 
-      get name() {
-        return this._name
-      }
+  get name() {
+    return this._name
+  }
 
-      get exports() {
-        return this._exports
-      }
+  get exports() {
+    return this._exports
+  }
 
-      unload() {
-        let unloaded = false
+  unload() {
+    let unloaded = false
 
-        if (this._handle) {
-          unloaded = bare.unloadAddon(this._handle)
-          this._handle = null
-        }
+    if (this._handle) {
+      unloaded = bare.unloadAddon(this._handle)
+      this._handle = null
+    }
 
-        Addon._addons.delete(this)
+    Addon._addons.delete(this)
 
-        return unloaded
-      }
+    return unloaded
+  }
 
-      [Symbol.for('bare.inspect')]() {
-        return {
-          __proto__: { constructor: Addon },
+  [Symbol.for('bare.inspect')]() {
+    return {
+      __proto__: { constructor: Addon },
 
-          url: this.url,
-          name: this._name,
-          exports: this.exports
-        }
-      }
+      url: this.url,
+      name: this._name,
+      exports: this.exports
+    }
+  }
 
-      static _protocol = Module._protocol
-      static _cache = Object.create(null)
-      static _addons = new Set()
-      static _builtins = bare.getStaticAddons()
-      static _conditions = Module._conditions
+  static _protocol = Module._protocol
+  static _cache = Object.create(null)
+  static _addons = new Set()
+  static _builtins = bare.getStaticAddons()
+  static _conditions = Module._conditions
 
-      static get cache() {
-        return this._cache
-      }
+  static get cache() {
+    return this._cache
+  }
 
-      static get host() {
-        return `${bare.platform}-${bare.arch}${bare.simulator ? '-simulator' : ''}`
-      }
+  static get host() {
+    return `${bare.platform}-${bare.arch}${bare.simulator ? '-simulator' : ''}`
+  }
 
-      static load(url, opts = {}) {
-        const self = Addon
+  static load(url, opts = {}) {
+    const self = Addon
 
-        const {
-          referrer = null,
-          protocol = referrer ? referrer._protocol : self._protocol,
-          resolutions = referrer ? referrer._resolutions : null
-        } = opts
+    const {
+      referrer = null,
+      protocol = referrer ? referrer._protocol : self._protocol,
+      resolutions = referrer ? referrer._resolutions : null
+    } = opts
 
-        const cache = self._cache
+    const cache = self._cache
 
-        let addon = cache[url.href] || null
+    let addon = cache[url.href] || null
 
-        if (addon !== null) return addon
+    if (addon !== null) return addon
 
-        addon = cache[url.href] = new Addon(url)
+    addon = cache[url.href] = new Addon(url)
 
-        try {
-          switch (url.protocol) {
-            case 'builtin:':
-              addon._handle = bare.loadStaticAddon(url.pathname)
-              break
-            case 'linked:':
-              addon._handle = bare.loadDynamicAddon(url.pathname, addon._name)
-              break
-            case 'file:':
-              for (const packageURL of lookupPackageScope(url, {
-                resolutions
-              })) {
-                if (protocol.exists(packageURL)) {
-                  addon._name = addonName(
-                    Module.load(packageURL, { protocol })._exports
-                  )
-                  break
-                }
-              }
-
-              addon._handle = bare.loadDynamicAddon(
-                fileURLToPath(url),
-                addon._name
+    try {
+      switch (url.protocol) {
+        case 'builtin:':
+          addon._handle = bare.loadStaticAddon(url.pathname)
+          break
+        case 'linked:':
+          addon._handle = bare.loadDynamicAddon(url.pathname, addon._name)
+          break
+        case 'file:':
+          for (const packageURL of lookupPackageScope(url, {
+            resolutions
+          })) {
+            if (protocol.exists(packageURL)) {
+              addon._name = addonName(
+                Module.load(packageURL, { protocol })._exports
               )
               break
-            default:
-              throw AddonError.UNSUPPORTED_PROTOCOL(
-                `Unsupported protocol '${url.protocol}' for addon '${url.href}'`
-              )
+            }
           }
 
-          addon._exports = bare.initAddon(addon._handle, addon._exports)
-        } catch (err) {
-          addon.unload()
-
-          delete cache[url.href]
-
-          throw err
-        }
-
-        return addon
+          addon._handle = bare.loadDynamicAddon(
+            fileURLToPath(url),
+            addon._name
+          )
+          break
+        default:
+          throw AddonError.UNSUPPORTED_PROTOCOL(
+            `Unsupported protocol '${url.protocol}' for addon '${url.href}'`
+          )
       }
 
-      static unload(url) {
-        const self = Addon
+      addon._exports = bare.initAddon(addon._handle, addon._exports)
+    } catch (err) {
+      addon.unload()
 
-        const cache = self._cache
+      delete cache[url.href]
 
-        const addon = cache[url.href] || null
+      throw err
+    }
 
-        if (addon === null) {
-          throw AddonError.ADDON_NOT_FOUND(`Cannot find addon '${url.href}'`)
-        }
+    return addon
+  }
 
-        const unloaded = addon.unload()
+  static unload(url) {
+    const self = Addon
 
-        if (unloaded) delete cache[url.href]
+    const cache = self._cache
 
-        return unloaded
-      }
+    const addon = cache[url.href] || null
 
-      static resolve(specifier, parentURL, opts = {}) {
-        const self = Addon
+    if (addon === null) {
+      throw AddonError.ADDON_NOT_FOUND(`Cannot find addon '${url.href}'`)
+    }
 
-        const {
-          referrer = null,
-          protocol = referrer ? referrer._protocol : self._protocol,
-          imports = referrer ? referrer._imports : null,
-          resolutions = referrer ? referrer._resolutions : null,
-          builtins = self._builtins,
-          conditions = referrer ? referrer._conditions : self._conditions
-        } = opts
+    const unloaded = addon.unload()
 
-        const resolved = protocol.preresolve(specifier, parentURL)
+    if (unloaded) delete cache[url.href]
 
-        const [resolution] = protocol.resolve(resolved, parentURL, imports)
+    return unloaded
+  }
 
-        if (resolution) return protocol.postresolve(resolution, parentURL)
+  static resolve(specifier, parentURL, opts = {}) {
+    const self = Addon
 
-        for (const resolution of resolve(
-          resolved,
-          parentURL,
-          {
-            conditions: ['addon', ...conditions],
-            host: self.host,
-            resolutions,
-            builtins,
-            extensions: ['.bare', '.node']
-          },
-          readPackage
-        )) {
-          switch (resolution.protocol) {
-            case 'builtin:':
-              return resolution
-            case 'linked:':
-              try {
-                return Addon.load(resolution, opts).url
-              } catch {
-                continue
-              }
-            default:
-              if (protocol.exists(resolution)) {
-                return protocol.postresolve(
-                  protocol.addon ? protocol.addon(resolution) : resolution
-                )
-              }
+    const {
+      referrer = null,
+      protocol = referrer ? referrer._protocol : self._protocol,
+      imports = referrer ? referrer._imports : null,
+      resolutions = referrer ? referrer._resolutions : null,
+      builtins = self._builtins,
+      conditions = referrer ? referrer._conditions : self._conditions
+    } = opts
+
+    const resolved = protocol.preresolve(specifier, parentURL)
+
+    const [resolution] = protocol.resolve(resolved, parentURL, imports)
+
+    if (resolution) return protocol.postresolve(resolution, parentURL)
+
+    for (const resolution of resolve(
+      resolved,
+      parentURL,
+      {
+        conditions: ['addon', ...conditions],
+        host: self.host,
+        resolutions,
+        builtins,
+        extensions: ['.bare', '.node']
+      },
+      readPackage
+    )) {
+      switch (resolution.protocol) {
+        case 'builtin:':
+          return resolution
+        case 'linked:':
+          try {
+            return Addon.load(resolution, opts).url
+          } catch {
+            continue
           }
-        }
-
-        throw AddonError.ADDON_NOT_FOUND(
-          `Cannot find addon '${specifier}' imported from '${parentURL.href}'`
-        )
-
-        function readPackage(packageURL) {
-          if (protocol.exists(packageURL)) {
-            return Module.load(packageURL, { protocol })._exports
+        default:
+          if (protocol.exists(resolution)) {
+            return protocol.postresolve(
+              protocol.addon ? protocol.addon(resolution) : resolution
+            )
           }
-
-          return null
-        }
       }
-    })
+    }
+
+    throw AddonError.ADDON_NOT_FOUND(
+      `Cannot find addon '${specifier}' imported from '${parentURL.href}'`
+    )
+
+    function readPackage(packageURL) {
+      if (protocol.exists(packageURL)) {
+        return Module.load(packageURL, { protocol })._exports
+      }
+
+      return null
+    }
+  }
+}
 
 function addonName(info) {
   if (typeof info !== 'object' || info === null) return null
@@ -224,7 +221,7 @@ function addonName(info) {
 }
 
 Bare.prependListener('teardown', () => {
-  for (const addon of Addon._addons) {
+  for (const addon of exports._addons) {
     addon.unload()
   }
 })
