077a4ef0e061985eb3c17cbcb35de2b8cc8f3aaa
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-21 09:50:40 +0100

    Format addons

--- test/fixtures/dependent-addon/a/binding.c
+++ test/fixtures/dependent-addon/a/binding.c
@@ -3,12 +3,12 @@
 #include <js.h>
 
 int
-addon_a_fn (void) {
+addon_a_fn(void) {
   return 42;
 }
 
 static js_value_t *
-addon_a_exports (js_env_t *env, js_value_t *exports) {
+addon_a_exports(js_env_t *env, js_value_t *exports) {
   int err = js_create_int32(env, addon_a_fn(), &exports);
   assert(err == 0);
 
--- test/fixtures/dependent-addon/b/binding.c
+++ test/fixtures/dependent-addon/b/binding.c
@@ -3,10 +3,10 @@
 #include <js.h>
 
 int
-addon_a_fn (void);
+addon_a_fn(void);
 
 static js_value_t *
-addon_b_exports (js_env_t *env, js_value_t *exports) {
+addon_b_exports(js_env_t *env, js_value_t *exports) {
   int err = js_create_int32(env, addon_a_fn(), &exports);
   assert(err == 0);
 
