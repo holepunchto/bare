a068157c2064799c61740cd1263b398e1e43d7e6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-23 14:54:08 +0100

    Update dependencies

--- src/globals.js
+++ src/globals.js
@@ -7,75 +7,21 @@
  * Node.js.
  */
 
-/**
- * https://developer.mozilla.org/en-US/docs/Web/API/queueMicrotask
- */
-
-const resolved = Promise.resolve()
-
-const crash = (err) =>
-  setImmediate(() => {
-    throw err
-  })
-
-global.queueMicrotask = function queueMicrotask(fn) {
-  resolved
-    .then(fn)
-    // Make sure that exceptions are reported as normal uncaughts, not promise
-    // rejections.
-    .catch(crash)
-}
-
-/**
- * https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
- */
-
-global.structuredClone = require('bare-structured-clone')
-
-/**
- * Thanks to Node.js, it is customary for the buffer API to be available
- * globally and many of the core modules in Bare also assume this.
- */
-
-global.Buffer = require('bare-buffer')
-
-/**
- * Make timers globally available, including the Node.js-specific immediate
- * API which schedules events to run after each I/O tick.
- */
-
-const timers = require('bare-timers')
-
-global.setTimeout = timers.setTimeout
-global.clearTimeout = timers.clearTimeout
-
-global.setInterval = timers.setInterval
-global.clearInterval = timers.clearInterval
-
-global.setImmediate = timers.setImmediate
-global.clearImmediate = timers.clearImmediate
-
-/**
- * Make the debugging console globally available.
- */
+require('bare-queue-microtask/global')
+require('bare-buffer/global')
+require('bare-timers/global')
+require('bare-structured-clone/global')
+require('bare-url/global')
 
 const Console = require('bare-console')
 
 global.console = new Console({
   colors: bare.isTTY,
   bind: true,
-
   stdout(data) {
-    bare.printInfo(data.replace(/\u0000/g, '\\x00')) // eslint-disable-line no-control-regex
+    bare.printInfo(data.replace(/\u0000/g, '\\x00'))
   },
-
   stderr(data) {
-    bare.printError(data.replace(/\u0000/g, '\\x00')) // eslint-disable-line no-control-regex
+    bare.printError(data.replace(/\u0000/g, '\\x00'))
   }
 })
-
-/**
- * Make the URL constructor globally available.
- */
-
-global.URL = require('bare-url').URL
