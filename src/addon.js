58b8a094c2462daf87e3de21a0db09f47a646007
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-12-13 11:51:00 +0100

    Fix scoped addon naming

--- src/addon.js
+++ src/addon.js
@@ -215,7 +215,7 @@ function addonName(info) {
 
   const major = version.substring(0, version.indexOf('.'))
 
-  return name.replace(/\//g, '+') + '@' + major + '.bare'
+  return name.replace(/\//g, '+').replace(/^@/, '') + '@' + major + '.bare'
 }
 
 Bare.prependListener('teardown', () => {
