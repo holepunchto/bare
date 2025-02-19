ef27fc66ce5757402026782958826862670083e3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-02-19 14:04:10 +0100

    Avoid TLA in test

--- test/import-addon.mjs
+++ test/import-addon.mjs
@@ -1,8 +1,12 @@
 import assert from 'bare-assert'
 const { Addon } = Bare
 
-const { default: mod } = await import(
-  `./fixtures/addon/prebuilds/${Addon.host}/addon.bare`
-)
+async function main() {
+  const { default: mod } = await import(
+    `./fixtures/addon/prebuilds/${Addon.host}/addon.bare`
+  )
 
-assert(mod === 'Hello from addon')
+  assert(mod === 'Hello from addon')
+}
+
+main()
