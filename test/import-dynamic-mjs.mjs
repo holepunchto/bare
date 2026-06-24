d6886e3197cf9b9d4b01403ea0788c6bc82966f2
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-06-24 14:03:23 +0200

    Support TypeScript modules

--- test/import-dynamic-mjs.mjs
+++ test/import-dynamic-mjs.mjs
@@ -2,4 +2,4 @@ import t from 'bare-tap'
 
 t.plan(1)
 
-import('./fixtures/esm').then(({ default: mod }) => t.equal(mod, 'Hello from ESM'))
+import('./fixtures/mjs').then(({ default: mod }) => t.equal(mod, 'Hello from ESM'))
